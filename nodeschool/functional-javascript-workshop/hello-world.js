8dcc5703eed8ad7bb6f04964e3f57bced2bbdaf8
GoobleJook <robert@smallbizninja.com>
2016-06-09 16:36:43 -0400

    Add node school exercises

---
+++ nodeschool/functional-javascript-workshop/hello-world.js
@@ -0,0 +1,10 @@
+function upperCaser (input) {
+	return input.toUpperCase();
+}
+
+module.exports = upperCaser
+
+// Official solution:
+// module.exports = function (import) {
+// 	return input.toUpperCase();
+// }
---
+++ nodeschool/javascripting/if-statement.js
@@ -0,0 +1,7 @@
+var fruit = "orange";
+var fruit_length = fruit.length;
+if (fruit_length > 5) {
+	console.log("The fruit name has more than five characters.");
+} else {
+	console.log("The fruit name has five characters or less.");
+}
