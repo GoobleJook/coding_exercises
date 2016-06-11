157c5aefe430248e572221adb596a0f427310794
GoobleJook <robert@smallbizninja.com>
2016-06-11 16:42:54 -0400

    Add Advanced JavaScript day 3 exercises/examples

--- frontendmasters/scope.js
+++ frontendmasters/scope.js
@@ -1,6 +1,6 @@
 var foo = "bar";  // this is 2 operations: a declaration statement (var foo registered into global scope) and an initilization statment (foo = "bar")
 
-function bar() {
+function bar()  {
 	var foo = "baz"; // declare foo in scope of bar
 } // JIT (just in time) compliation -- compiles function bar but doesn't worry about content, until bar is called
 
@@ -17,3 +17,68 @@ function baz(foo) {  // declare baz into global scope, then there is a declared 
 // LHS (left hand side of assignment/target) = foo
 	// - does this variable exist? (looking it up by asking scope manager, answer: yes, you declared him in global scope; no need for RHS lookup since it's already declared)?
 // RHS (right hand side of assignment/source) = "bar"
+
+// Day 3: Function Declarations, Functions Expressions, and Block Scope
+
+// lesson 3, example 1:
+
+var foo = function bar () { // This is not a declaration, but an expression. Declaration begin with the word function.
+	var foo = "baz";
+
+	function baz(foo) {
+		foo = bar;
+		foo;
+	}
+	baz();
+};
+
+foo();
+bar(); //error, does not exist outside of itself.
+
+// Three negative things about anonymous function expressions:
+// 1. We have no way inside of the function to refer to ourself
+// 2. Anonymous fucntions don't play well in debugging (without a name, can't be used in debug stack traces).
+// 3. It self-documents code.
+
+Lesson 3, example 2:
+
+var foo;
+
+try {
+	foo.length;
+}
+catch (err) { // As of ES3 catch clause is block-scoped.
+	console.log(err); // TypeError
+}
+
+console.log(err); // ReferenceError
+
+// Day 3, lessson 2: Lexical scope
+
+// Bash scripting, opt-in Perl use dyanmical scoping
+// Lexical scope means compile-time scope, scoping decisions are made in stone when you compile
+// Lexical scope starts at "first floor of building"
+// if not found there, it moves up a "floor" (out a level) -- Global scope is top level
+
+function foo () {
+	var bar = "bar";
+
+	function baz() {
+		console.log(bar); //lexical! author-time decision, can't change
+	}
+	baz(); 
+}
+foo();
+
+// Day 3, lessone 3: Cheating lexical scope: eval
+
+var bar = "bar";
+
+function foo(str) {
+	eval(str); //cheating, evaluates a string/treats like it's code
+	console.log(bar); //42
+}
+
+foo("var bar = 42;");
+
+// cheating makes your JS run slow
