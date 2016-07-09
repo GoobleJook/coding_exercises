932caa37e25e488a57915842d9a678cf09b72216
GoobleJook <robert@smallbizninja.com>
2016-07-09 18:38:25 -0400

    Move mapStyle to mapstyle.js; Add listener to alert when map is ready

--- JavaScript/sitepoint-introduction-to-google-maps-api-master/exercise/js/scripts.js
+++ JavaScript/sitepoint-introduction-to-google-maps-api-master/exercise/js/scripts.js
@@ -4,43 +4,6 @@ var map; // Global map variable
 var lat = document.getElementById('latcoords');
 var lng = document.getElementById('loncoords');
 
-var mapStyle = [
-	{
-		'stylers':[
-			{'saturation':-100},
-			{'gamma':1}
-		]
-	},
-	{
-		'elementType': 'labels.text.stroke',
-		'stylers':[
-			{'visibility':'on'}
-		]
-	},
-	{
-		'featureType':'road',
-		'elementType':'geometry',
-		'stylers':[
-			{'visibility':'simplified'}
-		]
-	},
-	{
-		'featureType':'water',
-		'stylers':[
-			{'visibility':'on'},
-			{'saturation':50},
-			{'gamma':0},
-			{'hue':'#50a5d1'}
-		]
-	},
-	{
-		'featureType':'landscape',
-		'elementType':'all',
-		'stylers':[
-			{'color':'#e2e2e2'}
-		]
-	}
-];
 
 //function run on DOM load
 function loadMap() {
@@ -73,7 +36,9 @@ function loadMap() {
 
 		styles: unsaturatedBrowns
 	};
+
 	var mapId = document.getElementById('map'); 
+	
 	map = new google.maps.Map(mapId, mapOptions); // Create the map
 
 	updateCurrentLatLng(map.getCenter());
@@ -81,9 +46,19 @@ function loadMap() {
 }
 
 function mapEventListeners() {
-	var mouseMoveChanged = google.maps.event.addListener(map, 'mousemove', function(event) {
-		updateCurrentLatLng(event.latLng);
-	})
+	var mouseMoveChanged = google.maps.event.addListener(map, 'mousemove', 
+		function(event) {
+			updateCurrentLatLng(event.latLng);
+		}
+	);
+
+	var listenerIdle = google.maps.event.addListenerOnce(map, 'idle', 
+		function() {
+			alert('Map is ready!');	
+		}
+	);
+		
+
 }  // Mouse move updates the coordinates
 
 // Update the position of the mouse in the latitude and longitude
