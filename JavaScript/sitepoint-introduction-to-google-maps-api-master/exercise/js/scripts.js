165d0065ec2c206e0fd006f02a2fac600a47be83
GoobleJook <robert@smallbizninja.com>
2016-07-09 18:54:12 -0400

    Left off problems with pushState (line 83)

--- JavaScript/sitepoint-introduction-to-google-maps-api-master/exercise/js/scripts.js
+++ JavaScript/sitepoint-introduction-to-google-maps-api-master/exercise/js/scripts.js
@@ -50,22 +50,37 @@ function mapEventListeners() {
 		function(event) {
 			updateCurrentLatLng(event.latLng);
 		}
-	);
+	); // Mouse move updates the coordinates
 
 	// var listenerIdle = google.maps.event.addListenerOnce(map, 'idle', 
 	// 	function() {
 	// 		alert('Map is ready!');	
 	// 	}
 	// );
+
+	var listenerDragEnd = google.maps.event.addListener(map, 'dragend', 
+		function() {
+			updateUrlLocation(map.getCenter(), map.getZoom());
+		}
+	); // drag end for updating url
+
+	var listenerZoomChanged = google.maps.event.addListener(map, 'zoom_changed', 
+		function() {
+			updateUrlLocation(map.getCenter(), map.getZoom());
+		}
+	); // zoom changed for updating URL
 		
-
-}  // Mouse move updates the coordinates
-
-// Update the position of the mouse in the latitude and longitude
+}  
 
 function updateCurrentLatLng(latLng) {
 	lat.innerHTML = latLng.lat();
 	lng.innerHTML = latLng.lng();
-}
+} // Update the position of the mouse in the latitude and longitude
+
+
+function updateUrlLocation(center, zoom) {
+	var url = '?lat='+center.lat()+'&lon='+center.lng()+'&zoom='+zoom;
+	window.history.pushState({center: "center", zoom: "zoom" }, 'map center', url);
+} // set lat and lon and zoom into url
 
 google.maps.event.addDomListener(window, 'load', loadMap()); // Load the map
