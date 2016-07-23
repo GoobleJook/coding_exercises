375581c409f5c4e2e37ceaeb2690e4a81e969bd3
GoobleJook <robert@smallbizninja.com>
2016-07-23 19:28:33 -0400

    Complete closure exercise 1

--- JavaScript/frontendmasters_js_fundamentals_to_functional/functionalJS-master/closures/closure_exercises.js
+++ JavaScript/frontendmasters_js_fundamentals_to_functional/functionalJS-master/closures/closure_exercises.js
@@ -2,12 +2,16 @@
 // another function, `blab` which alerts `string` and is immediately called inside 
 // the function `nonsense`. `blab` should look like this inside of the `nonsense` function:
 
+// 2. In your function, `nonsense`, change the immediate call to a setTimeout so that the
+//  call to `blab` comes after 2 seconds. The `blab` function itself should stay the same as before.
+
 var nonsense = function(string) {
-	var blab = function(){
+	var blab = function(string){
 		alert(string);
 	};
-	return blab;
+	return blab(string);
 };
 
 stringAlert = nonsense('stringAlert');
 console.log(stringAlert());
+
