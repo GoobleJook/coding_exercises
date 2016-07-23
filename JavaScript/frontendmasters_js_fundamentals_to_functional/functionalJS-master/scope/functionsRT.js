5087c1b872d11a61abe6b85222a0c5dfa664261c
GoobleJook <robert@smallbizninja.com>
2016-07-23 12:38:55 -0400

    Play with ‘a new variable scope is created for every call to a function, as exemplified with a counter’

--- JavaScript/frontendmasters_js_fundamentals_to_functional/functionalJS-master/scope/functionsRT.js
+++ JavaScript/frontendmasters_js_fundamentals_to_functional/functionalJS-master/scope/functionsRT.js
@@ -10,3 +10,15 @@ var firstFn = function () {
 };
 
 firstFn();
+
+
+var fn = function () {
+  var innerCounter = innerCounter || 10;
+  innerCounter = innerCounter + 1;
+  ACTUAL = innerCounter;
+};
+
+fn();
+console.log(ACTUAL);
+fn();
+console.log(ACTUAL); // it is 11 both times because it is only initialized on each call, hence set to 10 on each call.
