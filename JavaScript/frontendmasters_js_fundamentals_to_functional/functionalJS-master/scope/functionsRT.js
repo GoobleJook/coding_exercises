fd756d370ae834dc7ba4f9964190dad6c1da680a
GoobleJook <robert@smallbizninja.com>
2016-07-23 12:26:21 -0400

    Play with a ‘function's local scope variables are not available anywhere outside that function, regardless of the context it's called in’ exercise

--- JavaScript/frontendmasters_js_fundamentals_to_functional/functionalJS-master/scope/functionsRT.js
+++ JavaScript/frontendmasters_js_fundamentals_to_functional/functionalJS-master/scope/functionsRT.js
@@ -1,1 +1,12 @@
-functionsRT.js
+var ACTUAL;
+ACTUAL = null;
+
+var firstFn = function () {
+	var localToFirstFn = 'first';
+	var secondFn = function () {
+		ACTUAL = localToFirstFn;
+	}
+	console.log(ACTUAL);
+};
+
+firstFn();
