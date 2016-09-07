ac912fdf19dfb049b30e3d4181c19effdb5c09d3
GoobleJook <robert@smallbizninja.com>
2016-09-06 20:57:37 -0400

    Move some files

---
+++ JavaScript/getify-es6-workshop/fat_arrow.js
@@ -0,0 +1,22 @@
+// Kyle is not a fan of arrow functions, because:
+// 1. Can be variation in syntax, unlike the standard function
+
+// foo = () => 2;
+
+// same as 
+
+// function foo () {
+// 	return 2;
+// }
+
+// some variations:
+// () => 3 -- these indicate parameters not function
+// x => 3
+// (...x) => 3
+// (x,y) => 3
+
+// Concise function body (all legal expressions allowed; statements are not expressions)
+
+// x => { try { 3; } catch(e) {} }
+
+// Arrow functions are syntacticly anonymous, so no named functions
