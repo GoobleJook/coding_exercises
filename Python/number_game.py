b8d4cde974777c534b1a1b52a560ee20a85d1440
GoobleJook <robert@smallbizninja.com>
2016-10-07 18:06:41 -0400

    Refactor number game

--- Python/number_game.py
+++ Python/number_game.py
@@ -2,13 +2,27 @@ import random
 
 # generate a random number from 1 - 10
 secret_num = random.randint(1, 10)
+guesses = []
 
 while True:
   # Get a number guess from a player
   guess = int(input('Guess a number between 1-10: '))
+  if guess in guesses:
+    print('You\'ve used that already!')
+    print('Please try a different number.')
+    break
+  else:
+    guesses.append(guess)
+  
   # Compare guess to secret number
   if guess == secret_num:
     print('You got it! My number was {}'.format(secret_num))
     break
-# print hit/miss
-  print('That\'s not it')
+  elif guess < secret_num:
+    position = "higher"
+  else:
+    position = "lower"
+  
+  print("That's not it. My number is {} than {}.".format(position,guess))
+  print('Please try again.')
+
