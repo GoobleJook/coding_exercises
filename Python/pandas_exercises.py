5fc7d7c1eeb7d194a3291fb6857ca7e0ce533a24
GoobleJook <robert@smallbizninja.com>
2016-11-04 13:58:06 -0400

    Left off Pandas version of None

--- Python/pandas_exercises.py
+++ Python/pandas_exercises.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+""" Series offer significant speed increases of Python's standard lists """
+
 animals = ['Tiger', 'Lion', 'Baer']
 animalSeries = pd.Series(animals)
 print(animalSeries)
@@ -7,3 +9,5 @@ print(animalSeries)
 numbers = [1, 2, 3]
 numberSeries = pd.Series(numbers)
 print(numberSeries)
+
+""" What do we do with missing data, as opposed to Python's None type? """
