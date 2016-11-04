abdd077a6baffb06045deb6aaf2eaaf731746442
GoobleJook <robert@smallbizninja.com>
2016-11-04 13:19:23 -0400

    Initial commit

---
+++ Python/pandas_exercises.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+animals = ['Tiger', 'Lion', 'Baer']
+animalSeries = pd.Series(animals)
+print(animalSeries)
+
+numbers = [1, 2, 3]
+numberSeries = pd.Series(numbers)
+print(numberSeries)
