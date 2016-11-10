5071f31b21b2983f6d95349e7ada75ac02c9379a
GoobleJook <robert@smallbizninja.com>
2016-11-10 10:52:12 -0500

    Add simple use of falsey in Python

---
+++ Python/automate_the_boring_stuff/ch02_flow_control/falsey.py
@@ -0,0 +1,11 @@
+name = ''
+while not name:		# This uses the Python logic that an empty string ('') is considered Falsey, that is, it is not True
+  print('Enter your name:')
+  name = input()
+print('How many guests will you have?')
+numOfGuests = int(input())
+if numOfGuests: # THis uses the same logic: If numOfGuests is not 0 it is True, so it will execute the print statement
+  print('Be sure to have enough room for your {} guests!'.format(numOfGuests))
+else:
+  print('Enjoy eating by yourself, loser!')
+
