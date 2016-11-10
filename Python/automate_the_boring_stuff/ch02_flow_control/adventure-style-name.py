aec47191f3db8afb8d7e41823639d76a825fd786
GoobleJook <robert@smallbizninja.com>
2016-11-10 10:24:34 -0500

    Broke game with break statement, which makes it work, hence no fun

--- Python/automate_the_boring_stuff/ch02_flow_control/adventure-style-name.py
+++ Python/automate_the_boring_stuff/ch02_flow_control/adventure-style-name.py
@@ -1,7 +1,9 @@
 # an annoying while loop in the style of the original "Adventure" game. "Kill the dragon? What?! With your bare hands?"
+# ruined with a break statement
+
 
 name = ''
 while name != 'your name':
 	name = input('Please enter your name: ')
 	print('Nice to meet you, {}.'.format(name))
-print('There you go, \'your name\', you finally got it. \'Member \'Adventure\'?')
+	break
