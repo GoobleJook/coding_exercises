7fe3f6d18f9254ba65931d3f964bac16caa5d861
GoobleJook <robert@smallbizninja.com>
2016-11-16 16:27:49 -0500

    Add dequeue, complete exercise

--- JavaScript/fcc/queue_exercise.js
+++ JavaScript/fcc/queue_exercise.js
@@ -11,11 +11,18 @@ class Queue{
   // create a way for people who click-buy to be added to the queue
   enqueue(val){
     this._storage[++this._end] = val;
-
     //++this._end increments the end variable first;
     //equivalent to 
     //this._end++ //->
     //this._storage[this._end] = val;
+  }
+
+  dequeue(){
+    if(this._end > this._start){ //check if there are values
+      let nextUp = this._storage[++this._start];
+      delete this._storage[this._start];
+      return nextUp;
+    }
   }
 
   size(){
@@ -24,3 +31,21 @@ class Queue{
 }
 
 let microsoftQueue = new Queue();
+
+microsoftQueue.enqueue("{user: ILoveWindows@gmail.com}")
+microsoftQueue.enqueue("{user: cortanaIsMyBestFriend@hotmail.com}")
+microsoftQueue.enqueue("{user: InternetExplorer8Fan@outlook.com}")
+microsoftQueue.enqueue("{user: IThrowApplesOutMyWindow@yahoo.com}")
+
+// console.log(microsoftQueue)
+
+//Function to send everyone their Surface Studio!
+let sendSurface = recepient => {
+  // sendTo(recepient);
+  console.log(recepient);
+}
+
+//When your server is ready to handle this queue, execute this:
+while(microsoftQueue.size() > 0){
+  sendSurface(microsoftQueue.dequeue());
+}
