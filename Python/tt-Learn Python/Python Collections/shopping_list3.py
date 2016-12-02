1475af57b73f4e87c6dd851c0c3334a68f7b62ed
GoobleJook <robert@smallbizninja.com>
2016-12-02 15:16:39 -0500

    Add my solution to using .pop() to add remove_item functionality to shopping_list.py

--- Python/tt-Learn Python/Python Collections/shopping_list3.py
+++ Python/tt-Learn Python/Python Collections/shopping_list3.py
@@ -1,8 +1,11 @@
 shopping_list = []
 
+def remove_item(idx):
+  shopping_list.pop(idx)
+
 def show_help():
   print('\nSepearate each item with a comma.')
-  print('Type DONE to quit, SHOW to see the current list and HELP to get this message.')
+  print('Type DONE to quit, SHOW to see the current list, REMOVE to take an item off the list, and HELP to get this message.')
 
 def show_list():
   count = 1
@@ -26,6 +29,16 @@ while True:
   elif new_stuff.upper() == 'HELP':
     show_help()
     continue
+  elif new_stuff.upper() == 'REMOVE':
+    index = input('What is the number of the item you want to remove? ')
+    try:
+      if index:
+        spot = int(index) - 1
+        remove_item(spot)
+    except:
+      print("That isn't a valid number. Please check again by typing 'SHOW', followed by 'REMOVE'")
+
+
   else:
     new_list = new_stuff.split(',')
     index = input('Add this at a certain spot? Press enter for the end of the list,'
