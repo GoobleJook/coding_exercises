3fd9a472c3131b3420eeeca070742d8f177a2fab
GoobleJook <robert@smallbizninja.com>
2016-12-08 10:30:16 -0500

    Initial commit tuples exercises

---
+++ Python/tt-Learn Python/Python Collections/tuples.py
@@ -0,0 +1,5 @@
+# Tuples are created not by parenthesis but by commas
+# Generally they are seen created with parenthesis; however, they aren't necessary
+# my_tup = (1,2,3) is the same as my_tup = 1,2,3
+# or, since Python is expecting an iteratible, you can feed it
+# my_tup = ([1,2,3]), which is the same as the above
