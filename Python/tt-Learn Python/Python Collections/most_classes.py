6bd35445d3d69228d26f07735e592f30e5be95f7
GoobleJook <robert@smallbizninja.com>
2016-12-08 10:16:02 -0500

    Complete teacher stats list challenge

--- Python/tt-Learn Python/Python Collections/most_classes.py
+++ Python/tt-Learn Python/Python Collections/most_classes.py
@@ -19,6 +19,19 @@ def num_teachers(teachers):
   return count
 
 
+# Now, create a function named stats that takes the teacher dictionary. 
+# Return a list of lists in the format [<teacher name>, <number of classes>]. 
+# For example, one item in the list would be ['Dave McFarland', 1].
+
+def stats(teachers):
+  stats_list = []
+  teacher_list = []
+  for teacher, classes in teachers.items():
+    teacher_list.append(teacher)
+    teacher_list.append(len(classes))
+    stats_list.append(teacher_list)
+    teacher_list = []
+  return stats_list
 
 
 
@@ -31,4 +44,4 @@ my_teachers = {
 
 print(most_classes(my_teachers))
 print(num_teachers(my_teachers))
-
+print(stats(my_teachers))
