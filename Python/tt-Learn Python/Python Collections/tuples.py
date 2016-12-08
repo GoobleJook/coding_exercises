7d53343d1223c6505d54105996202c3142a96642
GoobleJook <robert@smallbizninja.com>
2016-12-08 11:09:17 -0500

    Complete tuple notes up to tuple variable swapping

--- Python/tt-Learn Python/Python Collections/tuples.py
+++ Python/tt-Learn Python/Python Collections/tuples.py
@@ -3,3 +3,43 @@
 # my_tup = (1,2,3) is the same as my_tup = 1,2,3
 # or, since Python is expecting an iteratible, you can feed it
 # my_tup = ([1,2,3]), which is the same as the above
+
+# Tuples can be used for simultaneous assignment
+
+# a,b = 1,2
+
+# a >> 1
+# b >> 2
+
+# Why, because commas make Tuples not parenthesis
+
+# Tuple unpacking is pretty fucking neat
+
+# c = (3,4)
+
+# d, e = c
+# >> d
+# >> 3
+# >> e
+# >> 4
+
+# Tuple packing is pretty fucking cool too
+
+# f = (d, e)
+# >> f 
+# >> 3, 4
+
+# Tuple swapping is also neat
+
+# First reset a and b
+# >> del a
+# >> del b
+# >> a = 1
+# >> b = 2
+
+# Let's try to swap them. In many languages you need a third variable to do this. But in Python...
+# >> a,b = b,a
+# >> a
+# >> 2
+# >> b
+# >> 1
