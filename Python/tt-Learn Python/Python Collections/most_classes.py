20c706c60dc509cef1b2e30ed5b3cbfa24a7464e
GoobleJook <robert@smallbizninja.com>
2016-12-08 09:54:03 -0500

    Complete teacher count exercise

--- Python/tt-Learn Python/Python Collections/most_classes.py
+++ Python/tt-Learn Python/Python Collections/most_classes.py
@@ -1,4 +1,7 @@
+
+
 def most_classes(teachers):
+# returns name of the teacher who has taught the most classes
   longest = 0
   best = ''
   for teacher, classes in teachers.items():
@@ -8,10 +11,24 @@ def most_classes(teachers):
       best = teacher
   return best
 
+def num_teachers(teachers):
+# returns the total number of teachers in the dictionary
+  count = 0
+  for teacher in teachers.items():
+    count += 1
+  return count
+
+
+
+
+
 my_teachers = {
   'Smith': ['math','math','math'],
   'Jones': ['engrish','engrish'],
-  'Watson': ['detective stuff']
+  'Watson': ['detective stuff'],
+  'Pierre': ['kama','sutra','french','stuff']
 }
 
 print(most_classes(my_teachers))
+print(num_teachers(my_teachers))
+
