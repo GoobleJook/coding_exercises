cb6236ea887ae28c8a3ec80a8a5d0512ed9124e0
GoobleJook <robert@smallbizninja.com>
2016-12-08 06:12:59 -0500

    Complete shopping_list.py #1

--- Python/tt-Learn Python/Python Basics/shopping_list.py
+++ Python/tt-Learn Python/Python Basics/shopping_list.py
@@ -1,3 +1,9 @@
+# For this first pass, here's what I want to be able to do.
+# Run the script, to start using it.
+# Put new things into the list one thing at a time.
+# Enter the word done, in all caps, to quit the program.
+# And once I quit, I want the app to show me everything that's on my list.
+
 shopping_list = []
 
 while True:
@@ -8,6 +14,7 @@ while True:
     break
   shopping_list.append(user_input)
 item_num = 0
+print('Your shopping list currently contains:')
 for item in shopping_list:
   item_num += 1
-  print('{}: ').format(item_num)
+  print('{}: {}'.format(item_num, item))
