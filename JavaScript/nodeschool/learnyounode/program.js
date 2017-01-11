e591bcebeba54e7dfa738c61f79258ebfc43f9b1
GoobleJook <robert@smallbizninja.com>
2017-01-10 21:55:45 -0500

    Refactor, demonstrate my solution as less brittle than nodeschool’s

--- JavaScript/nodeschool/learnyounode/program.js
+++ JavaScript/nodeschool/learnyounode/program.js
@@ -1,10 +1,19 @@
-process.argv;
-var sum = 0;
+// process.argv; -- unnecessary, called in for loop
+var sum_of_argv_numbers = 0;
 
 for (var i = 0; i < process.argv.length; i++) {
   if(isNaN(process.argv[i]) === false) {
-    sum += Number(process.argv[i]);
+    sum_of_argv_numbers += Number(process.argv[i]);
   }
 }
 
-console.log(sum);
+console.log(sum_of_argv_numbers);
+
+// Their solution below seems brittle to me, as i is hardcoded but mine detects when the array item is a not NaN
+// var result = 0
+    
+//     for (var i = 2; i < process.argv.length; i++) {
+//       result += Number(process.argv[i])
+//     }
+    
+//     console.log(result)
