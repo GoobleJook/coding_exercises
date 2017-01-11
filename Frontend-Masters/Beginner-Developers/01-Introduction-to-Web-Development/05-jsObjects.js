13b272660e698aa7f0e829690e988973d3a7a570
GoobleJook <robert@smallbizninja.com>
2017-01-11 15:43:35 -0500

    Fail — Brain can’t hack .bind()

--- Frontend-Masters/Beginner-Developers/01-Introduction-to-Web-Development/05-jsObjects.js
+++ Frontend-Masters/Beginner-Developers/01-Introduction-to-Web-Development/05-jsObjects.js
@@ -3,11 +3,30 @@ var car = {
   model: "Model S",
   acceleration: 30,
   accelerate: function() {
-    this.accelerate += 10
+    this.acceleration += 10;
   }
 }
 
 console.log("I drive a " + car.make + " " + car.model +".");
 console.log("I am going " + car.acceleration + " MPH.");
-car.accelerate;
+car.accelerate();
 console.log("Vroom! Vroom! Now I am going " + car.acceleration + " MPH!")
+
+
+// Brian was unable to get this to work, and mentioned that Kyle covers
+// a lot of .bind() in the advanced JS classes
+
+// var person = {
+//   favorite: "Tesla Model Q",
+//   getFave: function() {
+//     return this.favorite;
+//   }
+// };
+
+// var man = {
+//   favorite: "Renault"
+// }
+
+// console.log(person.getFave());
+// var func = person.getFave;
+// console.log(func().bind(man));
