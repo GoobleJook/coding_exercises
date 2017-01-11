673f811890be758ea8d7b05a167bd7955e99bc2e
GoobleJook <robert@smallbizninja.com>
2017-01-10 21:59:42 -0500

    Document stop error in nodeschool solution

--- JavaScript/nodeschool/learnyounode/program.js
+++ JavaScript/nodeschool/learnyounode/program.js
@@ -10,6 +10,7 @@ for (var i = 0; i < process.argv.length; i++) {
 console.log(sum_of_argv_numbers);
 
 // Their solution below seems brittle to me, as i is hardcoded but mine detects when the array item is a not NaN
+// Their solution breaks if you input a data type other than a number after the first two items in argv
 // var result = 0
     
 //     for (var i = 2; i < process.argv.length; i++) {
