a32cbcae2b1af45787cede8ed0399a33606a70f3
GoobleJook <robert@smallbizninja.com>
2017-01-11 15:01:00 -0500

    Complete function exercise

--- Frontend-Masters/Beginner-Developers/01-Introduction-to-Web-Development/04-anatomyOfAFunction.js
+++ Frontend-Masters/Beginner-Developers/01-Introduction-to-Web-Development/04-anatomyOfAFunction.js
@@ -1,13 +1,22 @@
 var fruits = ["watermelon", "banana", "orange", "apple"];
 var cars = ["Chevy", "Ford", "Tesla"];
 var iceCream = ["vanilla", "chocolate"];
+var empty = [];
 
 var printList = function(list) {
-  for (var i=0; i<list.length; i++) {
-    console.log(list[i]);
-  };
+  if (list.length < 3) {
+    return "Not enough elements";
+  }
+  return list
 };
 
-printList(fruits);
-printList(cars);
-printList(iceCream);
+// console.log(printList(fruits));
+// console.log(printList(cars));
+// console.log(printList(iceCream)); 
+
+var concatLists = function(list1, list2) {
+  return list1.concat(list2)
+} 
+
+console.log(printList(concatLists(fruits,cars)));
+console.log(printList(concatLists(iceCream,empty)));
