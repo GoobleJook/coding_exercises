695f9650542459cdb388012128c15635f2085a0f
GoobleJook <robert@smallbizninja.com>
2017-01-11 16:04:29 -0500

    Finish anonymous functions

--- Frontend-Masters/Beginner-Developers/01-Introduction-to-Web-Development/06-anonymousFunctions.js
+++ Frontend-Masters/Beginner-Developers/01-Introduction-to-Web-Development/06-anonymousFunctions.js
@@ -10,6 +10,9 @@ var math = function(a, b, operate) {
   return operate(a,b);
 }
 
-console.log(subtract(1,3));
-console.log(add(1,3));
-console.log(math(1,23,add));
+console.log(subtract(1,3)); 
+console.log(add(1,3)); 
+console.log(math(1,23,add)); // passing a function as a variable
+console.log(math(100,2,function(a,b) {
+  return a * b;
+})); // passing an anonymous function inside a function
