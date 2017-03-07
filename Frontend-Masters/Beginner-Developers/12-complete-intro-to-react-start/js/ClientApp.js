0ba2b2e3dbfee7cc8972f1d3e3ee338ce5e0f515
GoobleJook <robert@smallbizninja.com>
2017-03-07 09:57:29 -0500

    Break with text version, start working with Brian through lectures

--- Frontend-Masters/Beginner-Developers/12-complete-intro-to-react-start/js/ClientApp.js
+++ Frontend-Masters/Beginner-Developers/12-complete-intro-to-react-start/js/ClientApp.js
@@ -1,4 +1,4 @@
-// global React ReactDOM
+/* global React ReactDOM */
 
 var div = React.DOM.div
 var h1 = React.DOM.h1
@@ -7,18 +7,25 @@ var MyTitle = React.createClass({
   render () {
     return (
       div(null,
-        h1(null, 'Check out this component!')
+        h1(null, this.props.title)
       )
     )
   }
 })
 
-var MyFirstComponent = (
-  div(null,
-    React.createElement(MyTitle, null),
-    React.createElement(MyTitle, null),
-    React.createElement(MyTitle, null)
-  )
-)
+ var MyTitleFact = React.createFactory(MyTitle)
+
+// replace render method
+var MyFirstComponent = React.createClass({
+  render () {
+    return (
+      div(null,
+        MyTitleFact({title: 'Props are great!'}),
+        MyTitleFact({title: 'Use props everywhere'}),
+        MyTitleFact({title: 'Props are the best!'})
+      )
+    )
+  }
+})
 
 ReactDOM.render(MyFirstComponent, document.getElementById('app'))
