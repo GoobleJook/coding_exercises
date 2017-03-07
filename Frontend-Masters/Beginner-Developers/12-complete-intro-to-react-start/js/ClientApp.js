58f1fdf027bab3f0bface8ea11eb722fb13556be
GoobleJook <robert@smallbizninja.com>
2017-03-07 10:13:38 -0500

    Start to separate code

--- Frontend-Masters/Beginner-Developers/12-complete-intro-to-react-start/js/ClientApp.js
+++ Frontend-Masters/Beginner-Developers/12-complete-intro-to-react-start/js/ClientApp.js
@@ -1,31 +1,14 @@
-/* global React ReactDOM */
-
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null,
-        h1(null, this.props.title)
-      )
-    )
-  }
-})
-
- var MyTitleFact = React.createFactory(MyTitle)
-
-// replace render method
 var MyFirstComponent = React.createClass({
-  render () {
+  render: function () {
     return (
       div(null,
-        MyTitleFact({title: 'Props are great!'}),
-        MyTitleFact({title: 'Use props everywhere'}),
-        MyTitleFact({title: 'Props are the best!'})
+        h1(null, 'This is my first component!')
       )
     )
   }
 })
 
-ReactDOM.render(MyFirstComponent, document.getElementById('app'))
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
