f2fc4ccbf1cad5a6aa67e0de1da8d691f82c8554
GoobleJook <robert@smallbizninja.com>
2017-03-08 11:49:52 -0500

    Add style-loader and css-loader to webpack.config.js

--- Frontend-Masters/Beginner-Developers/12-complete-intro-to-react-start/js/ClientApp.js
+++ Frontend-Masters/Beginner-Developers/12-complete-intro-to-react-start/js/ClientApp.js
@@ -2,18 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MyTitle from './MyTitle'
 
-var div = React.DOM.div
-
-var MyTitleFactory = React.createFactory(MyTitle)
-
 var MyFirstComponent = React.createClass({
   render: function () {
     return (
-      div(null,
-        MyTitleFactory({ title: 'Robert is a great coder', color: 'peru' }),
-        MyTitleFactory({ title: 'and his wife is an amazing lover', color: 'rebeccapurple' }),
-        MyTitleFactory({ title: 'He\'s happy to live in Paradise!', color: 'darkviolet' })
-      )
+      <div>
+        <MyTitle title='props are cool' color='rebeccapurple' />
+        <MyTitle title='Rob is cool' color='peru' />
+        <MyTitle title='props are proper' color='mediumaquamarine' />
+      </div>
     )
   }
 })
