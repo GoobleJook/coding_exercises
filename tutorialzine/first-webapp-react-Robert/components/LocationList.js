9184b5f59cbaecbd5f41c3fb2d2ceaa74041600a
GoobleJook <robert@smallbizninja.com>
2017-03-13 20:40:25 -0400

    Fix typos in LocationList.js

--- tutorialzine/first-webapp-react-Robert/components/LocationList.js
+++ tutorialzine/first-webapp-react-Robert/components/LocationList.js
@@ -3,22 +3,28 @@ var LocationItem = require('./LocationItem');
 
 var LocationList = React.createClass({
 
-  render () {
+  render(){
+    
     var self = this;
-    var locations = this.props.locations.map(function(1){
-      var active = self.props.activeLocationAddress == 1.address;
-      return <LocationItem address = {1.address} timestamp = {1.timestamp} active={active} onClick={self.props.onClick} />
-    })
+    
+    var locations = this.props.locations.map(function(l){
+    
+      var active = self.props.activeLocationAddress == l.address;
+      
+      return <LocationItem address={l.address} timestamp={l.timestamp} 
+        active={active} onClick={self.props.onClick} />
+    });
+
     if(!locations.length){
       return null;
     }
 
-    return {
+    return (
       <div className = 'list-group col-xs-12 col-md-6 col-md-offset-3'>
         <span className = 'list-grou-item active'>Saved Locations</span>
         {locations} 
       </div>
-    }
+    )
   }
 })
 
