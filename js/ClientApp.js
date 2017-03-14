aab2219e70acd1b08809438369ceaead77770e2a
GoobleJook <robert@smallbizninja.com>
2017-03-14 11:51:15 -0400

    Add script to package.json that run standard every time you run npm run lint

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -9,14 +9,13 @@ var MyTitle = React.createClass({
   render: function () {
     return (
       div(null,
-        h1({ style: { color: this.props.color, fontWeight: 'bold' }}, this.props.title)
+        h1({ style: { color: this.props.color, fontWeight: 'bold' } }, this.props.title)
       )
     )
   }
 })
 
 var MyTitleFactory = React.createFactory(MyTitle)
-
 
 var MyFirstComponent = React.createClass({
   render: function () {
@@ -29,6 +28,5 @@ var MyFirstComponent = React.createClass({
     )
   }
 })
-
 
 ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
