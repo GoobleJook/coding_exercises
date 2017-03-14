5a50618d213b3b609f6c40ad717977e9a2992312
GoobleJook <robert@smallbizninja.com>
2017-03-14 11:13:23 -0400

    Add createFactory() method to demonstrate titleFactory

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,11 +1,13 @@
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
+// Every component needs a render method, and it must be a pure function (doesn't modify any state or change anything), and it must return markup
+
 var MyTitle = React.createClass({
   render: function () {
     return (
       div(null,
-        h1({ style: { color: this.props.color } }, this.props.title)
+        h1(null, 'Check out my component!')
       )
     )
   }
@@ -13,17 +15,18 @@ var MyTitle = React.createClass({
 
 var MyTitleFactory = React.createFactory(MyTitle)
 
+
 var MyFirstComponent = React.createClass({
   render: function () {
     return (
       div(null,
-        MyTitleFactory({title: 'props are the best', color: 'peru'}),
-        MyTitleFactory({title: 'semicolons are the worst', color: 'mediumaquamarine'}),
-        MyTitleFactory({title: 'jklol its okay if you like semicolons', color: 'rebeccapurple'}),
-        MyTitleFactory({title: 'im out of ideas', color: 'dodgerblue'})
+        MyTitleFactory(MyTitle),
+        MyTitleFactory(MyTitle),
+        MyTitleFactory(MyTitle)
       )
     )
   }
 })
+
 
 ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
