032a1258c89955bbda8c81d58a5beab96e5acc74
GoobleJook <robert@smallbizninja.com>
2017-03-14 15:37:54 -0400

    Complete conversion to .jsx

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,18 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MyTitle from './MyTitle'
 
-var div = React.DOM.div
-
-var MyTitleFactory = React.createFactory(MyTitle)
-
 var MyFirstComponent = React.createClass({
   render: function () {
     return (
-      div(null,
-        MyTitleFactory({ title: 'props are the best', color: 'peru' }),
-        MyTitleFactory({ title: 'props are the worst', color: 'rebeccapurple' }),
-        MyTitleFactory({ title: 'props are meh', color: 'green' })
-     )
+      <div>
+        <MyTitle title='props are the best' color='peru' />
+        <MyTitle title='props are the worst' color='rebeccapurple' />
+        <MyTitle title='props are meh' color='green' />
+      </div>
     )
   }
 })
