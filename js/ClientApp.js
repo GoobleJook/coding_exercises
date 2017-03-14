0aac4a5c1e129f9c60cff74ebcf5bc464bcd93d0
GoobleJook <robert@smallbizninja.com>
2017-03-14 15:56:00 -0400

    Initial commit Search app

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import MyTitle from './MyTitle'
+import { render } from 'react-dom'
+import '../public/normalize.css'
+import '../public/style.css'
 
-var MyFirstComponent = React.createClass({
-  render: function () {
+const App = React.createClass({
+  render () {
     return (
-      <div>
-        <MyTitle title='props are the best' color='peru' />
-        <MyTitle title='props are the worst' color='rebeccapurple' />
-        <MyTitle title='props are meh' color='green' />
+      <div className='app'>
+        <div className='landing'>
+          <h1>Rob Video</h1>
+          <input type='text' placeholder='Search' />
+          <a>or Browse All the Things</a>
+        </div>
       </div>
     )
   }
 })
 
-ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
+render(<App />, document.getElementById('app'))
