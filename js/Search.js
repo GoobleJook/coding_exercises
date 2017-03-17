b563302aaf5821d32758a8951eeb16636f2cf489
GoobleJook <robert@smallbizninja.com>
2017-03-17 13:11:28 -0400

    “Break the browser” by adding non-state-changing string to input field as default

--- js/Search.js
+++ js/Search.js
@@ -3,12 +3,17 @@ import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'this is the default string'
+    }
+  },
   render () {
     return (
       <div className='search'>
         <header>
           <h1>Robert Video</h1>
-          <input type='text' placeholder='Search' />
+          <input value={this.state.searchTerm} type='text' placeholder='Search' />
         </header>
         <div>
           {preload.shows.map((show) => {
