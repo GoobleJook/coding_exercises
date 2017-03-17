fb82e804f762d5705cbea01ad2b72a95d560a9f5
GoobleJook <robert@smallbizninja.com>
2017-03-17 13:41:10 -0400

    Create “poor man’s search”

--- js/Search.js
+++ js/Search.js
@@ -19,11 +19,17 @@ const Search = React.createClass({
           <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
         </header>
         <div>
-          {preload.shows.map((show) => {
-            return (
-              <ShowCard key={show.imdbID} {...show} />
-            )
-          })}
+          {preload.shows
+            .filter((show) => {
+              return `${show.title} ${show.description}`
+              .toUpperCase().indexOf(this.state.searchTerm
+              .toUpperCase()) >= 0
+            })
+            .map((show) => {
+              return (
+                <ShowCard key={show.imdbID} {...show} />
+              )
+            })}
         </div>
       </div>
     )
