af886681d5f3bbfd2105d46419210c4e9f3b5c04
GoobleJook <robert@smallbizninja.com>
2017-04-18 11:07:32 -0400

    Add / to start of index.html script, create rough details component, add new route

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -16,7 +16,7 @@ test('Search should render a ShowCard for each show', () => {
   expect(component.find(ShowCard).length).toEqual(preload.shows.length)
 })
 
-test('Search should render correct amount of shows pasted on search', () => {
+test('Search should render correct amount of shows based on search', () => {
   const searchWord = 'house'
   const component = shallow(<Search />)
   component.find('input').simulate('change', {target: {value: searchWord}})
