092e85c6514efd8818b1e0efccf4ef0662dc244e
GoobleJook <robert@smallbizninja.com>
2017-04-20 12:45:14 -0400

    Debug, working

--- JavaScript/react-facebook-tutorial/src/index.js
+++ JavaScript/react-facebook-tutorial/src/index.js
@@ -7,7 +7,7 @@ function Square(props) {
     <button className="square" onClick={() => props.onClick()}>
         {props.value}
       </button>
-    )
+    );
 } // For classes that only contain a render(), it is easier to set them up as a function, like with Square
 
 class Board extends React.Component {
@@ -45,12 +45,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
-      xIsNext: true
+      stepNumber:0,
+      xIsNext: true,
     }
   }
   handleClick(i) {
-      const history = this.state.history
-      const current = history[history.length - 1]
+      var history = this.state.history.slice(0,this.state.stepNumber + 1)
+      var current = history[history.length - 1]
       const squares = current.squares.slice()
       if (calculateWinner(squares) || squares[i]) {
         return
@@ -60,12 +61,19 @@ class Game extends React.Component {
         history: history.concat([{
           squares: squares
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
       })
     }
+      jumpTo(step) {
+        this.setState({
+          stepNumber: step,
+          xIsNext: (step % 2) ? false : true,
+        })
+      }
   render() {
     const history = this.state.history
-    const current = history[history.length -1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
     
     let status
@@ -74,6 +82,17 @@ class Game extends React.Component {
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
     }
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Move #' + move :
+        'Game start'
+      return (
+        <li key={move}>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      )
+    })
     return (
       <div className="game">
         <div className="game-board">
@@ -81,7 +100,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
