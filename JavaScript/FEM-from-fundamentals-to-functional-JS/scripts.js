42cf330dbc5bbe5f89bcc22781622a2cd0b50123
GoobleJook <robert@smallbizninja.com>
2017-10-03 17:59:36 -0400

    First exercise complete, my attempt (ungraded)

--- JavaScript/FEM-from-fundamentals-to-functional-JS/scripts.js
+++ JavaScript/FEM-from-fundamentals-to-functional-JS/scripts.js
@@ -1,3 +1,26 @@
 //Use this file to implement Part One of your project
 
 var animal = {};
+
+animal.username = 'Harry';
+console.log(animal.username);
+
+animal['tagline'] = 'I\'m Harry!';
+console.log(animal['tagline']);
+
+var noises = [];
+animal.noises = noises;
+console.log(animal);
+
+var keyCount = 0;
+for (key in animal) {
+  keyCount ++;
+  if (key === 'username'){
+    console.log(`Hi my name is ${animal[key]}`);
+  } else if (key === 'tagline') {
+    console.log(`I like to say ${animal[key]}`);
+  }
+  return `Hi my name is ${animal[key]}`
+}
+
+console.log(`There are ${keyCount} keys in the animal object!`);
